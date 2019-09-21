39229626300c50a2ead9772326ec6a119240367e
Frank Cerny <frankk.cerny@gmail.com>
2019-09-21 11:04:17 -0400

    Adding demo script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # curly-waddle
+
+# Contains a script used in a demo for a presentation. Meant to exhibit the dangers that USB
+# Rubber Duckies can pose. The script is completely harmless. A python script that prints to the console.
---
+++ curly.py
@@ -0,0 +1,10 @@
+# A simple python script meant to show the potential damage a Rubber Ducky could do 
+# Frank Cerny
+# 9/20/19
+
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("You have been H4cked")
+print(ascii_banner)
+
+# print('\nYou\nhave\nbeen\nh4cked\n')
